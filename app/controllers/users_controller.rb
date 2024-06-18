b5601c88848c215abd71287ab76e3b5039e2b799
Haluk Can Sarıöz <76582367+halukcansarioz@users.noreply.github.com>
2024-06-18 17:12:23 +0300

    Step 8: Getting the albums on the ShowPage page from the api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  require 'httparty'
 
   def index
     @users = User.all
@@ -7,7 +8,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @address = @user.address
-    @albums = @user.albums
-  end
 
+    # API'den albümleri çekme
+    albums_response = HTTParty.get("https://jsonplaceholder.typicode.com/users/#{@user.id}/albums")
+    @albums = albums_response.parsed_response
+
+    # Albüm başına fotoğrafları çekme
+    @albums.each do |album|
+      photo_id = (album['id'].to_i - 1) * 50 + 5
+      album_photos_response = HTTParty.get("https://jsonplaceholder.typicode.com/photos?albumId=#{album['id']}&id=#{photo_id}")
+      album['photo'] = album_photos_response.parsed_response.first
+    end
+  end
 end
